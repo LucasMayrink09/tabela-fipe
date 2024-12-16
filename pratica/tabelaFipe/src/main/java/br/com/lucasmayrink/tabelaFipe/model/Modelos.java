1d2b7d3aa4697d27b82032f6dafbe884117c52fa
Lucas Mayrink Oliveira <160883390+LucasMayrink09@users.noreply.github.com>
2024-12-16 17:29:40 -0300

    Add files via upload

---
+++ pratica/tabelaFipe/src/main/java/br/com/lucasmayrink/tabelaFipe/model/Modelos.java
@@ -0,0 +1,9 @@
+package br.com.lucasmayrink.tabelaFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Modelos(List<Dados> modelos) {
+}
